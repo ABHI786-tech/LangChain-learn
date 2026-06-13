b3eac694516837a489041d86c819b9f4b0f61ee8
ABHI786-tech <abhiofficial453@gmail.com>
2026-06-13 23:20:28 +0530

    feat: strucutred_output/output_parser/chains

--- 3.strucutre_outout/with_strucutred_output.py
+++ 3.strucutre_outout/with_strucutred_output.py
@@ -10,7 +10,6 @@ load_dotenv()
 llm = HuggingFaceEndpoint(
     repo_id="deepseek-ai/DeepSeek-V4-Flash",
     task="text-generation",
-    # huggingfacehub_api_token="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"
     huggingfacehub_api_token=os.getenv("HUGGINGFACEHUB_ACCESS_TOKEN"),
 )
 model = ChatHuggingFace(llm=llm)
